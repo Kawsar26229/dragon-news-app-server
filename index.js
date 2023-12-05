92998f015022787eb2e6db0e10ca55c80687ee31
Kawsar26229 <knkawsar26229@gmail.com>
2023-12-05 16:22:05 +0600

    news and category with id and all news by using conditions

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-const port = process.env.PORT || 5000
-const categories = require('./data/categories.json');
+const port = process.env.PORT || 5000;
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Dragon News is running')
-})
+app.get("/", (req, res) => {
+  res.send("Dragon News is running");
+});
 
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "0") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const singleNews = news.find((n) => n._id === id);
+  res.send(singleNews);
+});
 
 app.listen(port, () => {
-    console.log(`Dragon news app server is running on port ${port}`);
-})
+  console.log(`Dragon news app server is running on port ${port}`);
+});
